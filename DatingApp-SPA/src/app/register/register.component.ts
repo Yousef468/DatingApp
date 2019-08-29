625c4f04e7016c01f5bd7fb3da1bc4fb3c4c7d22
unknown <yjamali77@gmail.com>
2019-08-29 19:27:37 +0430

    Begin of Section 12

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -9,20 +10,27 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class RegisterComponent implements OnInit {
   model: any = {};
-  @Input() valuesFromHomeComponenet: any;
+  // @Input() valuesFromHomeComponenet: any;
   @Output() cancelRegister = new EventEmitter();
+  registerForm: FormGroup;
   constructor(private authService: AuthService, private alertify: AlertifyService) { }
 
   ngOnInit() {
+     this.registerForm = new FormGroup({
+       username: new FormControl(),
+       password: new FormControl(),
+       confirmPassword: new FormControl()
+     });
   }
   register() {
-    console.log(this.model);
-    this.authService.register(this.model).subscribe(
-      () => this.alertify.success('Register successfully'),
-      (error) => {
-        this.alertify.error(error);
-      }
-    );
+    // console.log(this.model);
+    // this.authService.register(this.model).subscribe(
+    //   () => this.alertify.success('Register successfully'),
+    //   (error) => {
+    //     this.alertify.error(error);
+    //   }
+    // );
+    console.log(this.registerForm.value);
   }
 
   cancel() {
